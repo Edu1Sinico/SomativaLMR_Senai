3866c253af24d6e5e39b99e25b9c73f0f0ce0a36
Edu1Sinico <EduardoSenai888@gmail.com>
2023-06-10 00:06:37 -0300

    Enviando o site para um novo repósitorio

---
+++ PagCafeMania/app.js
@@ -0,0 +1,21 @@
+function clickMenu() {
+    if (menuCelular.style.display == 'block') {
+        menuCelular.style.display = 'none'
+    }
+    else {
+        menuCelular.style.display = 'block'
+    }
+}
+
+function clickSearch() {
+    if (Search.style.display == 'block') {
+        Search.style.display = 'none'
+    }
+    else {
+        Search.style.display = 'block'
+    }
+}
+
+function clickLogin(){
+    window.location.href="login.html"
+}
