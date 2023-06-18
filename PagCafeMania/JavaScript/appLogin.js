f9c22fa91bfc7bd765c837dae5d22bfbb3fe0045
Edu1Sinico <EduardoSenai888@gmail.com>
2023-06-18 19:06:44 -0300

    Site finalizado :)

--- PagCafeMania/JavaScript/appLogin.js
+++ PagCafeMania/JavaScript/appLogin.js
@@ -1,17 +1,24 @@
+// tela de login
+
 function buttonLogin() {
     var nome = document.getElementById("pUser").value;
     var senha = document.getElementById("pSenha").value;
 
-    if (nome === 'adm1' && senha === '1234') {
-        window.location.href = "index.html";
-        localStorage.setItem('isLoggedIn', 'true');
+    if (nome !== '' && senha !== '') {
+        if (nome === 'adm1' && senha === '1234') {
+            window.location.href = "index.html";
+            localStorage.setItem('isLoggedIn', 'true');
+        }
+        else {
+            alert("Usuário ou senha incorretos.");
+        }
     }
-    else {
-        alert("Usuário ou senha incorretos.");
+    else{
+        alert("Preencha os campos.");
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     var isLoggedIn = localStorage.getItem('isLoggedIn');
     var loginButton = document.getElementById('buttonLoginCad');
     var verificarLogin = document.getElementById('verificarLogin');
@@ -22,8 +29,13 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 });
 
+// tela de index
+
 function clickLogout() {
     localStorage.setItem('isLoggedIn', 'false');
     window.location.href = "index.html";
 }
 
+function clickCad(){
+    window.location.href = "cad.html";
+}
